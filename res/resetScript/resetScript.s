11ae0f4beb223c03777a156c1dff2f293799a33d
Lawrence Warren <lawrencewarren2@gmail.com>
2021-02-02 17:28:22 +0000

    Added a reset script that sets all registers to 0

---
+++ res/resetScript/resetScript.s
@@ -0,0 +1,19 @@
+  B main
+  ALIGN
+main
+  MOV R0, #0
+  MOV R1, #0
+  MOV R2, #0
+  MOV R3, #0
+  MOV R4, #0
+  MOV R5, #0
+  MOV R6, #0
+  MOV R7, #0
+  MOV R8, #0
+  MOV R9, #0
+  MOV R10, #0
+  MOV R11, #0
+  MOV R12, #0
+  MOV R13, #0
+  MOV R14, #0
+  SWI 2
